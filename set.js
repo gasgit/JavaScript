535ae394f5060f7e94a47461b9a9e559814ebcff
gasgit <g00305555@gmit.ie>
2017-01-03 19:29:25 +0000

    set

---
+++ set.js
@@ -0,0 +1,10 @@
+// use strict required (of)
+
+"use strict";
+
+var oneHit = new Set();
+oneHit.add("Nena");
+oneHit.add("Vanilla Ice");
+oneHit.add("Dexy and ..");
+
+for(let item of oneHit) console.log(item);
